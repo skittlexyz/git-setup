922b0e8d424a00f888eab4b9bcb1cc5b747ef8c0
skittlexyz <mo644774@gmail.com>
2024-02-08 11:56:58 +0000

    erik diego commit

--- main.cpp
+++ main.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
-#include <string>
 #include <sstream>
+#include <string>
 
 /* -a --author | -e --email */
+/* CHANGES HERE */
 
-int handleParameters(int argc, char** argv) {
-  if (argc == 1) return -1;
+int handleParameters(int argc, char **argv) {
+  if (argc == 1)
+    return -1;
   return 0;
 }
 
 int main(int argc, char **argv) {
   int option = handleParameters(argc, argv);
   switch (option) {
-    case -1:
-      std::cout << "Usage: ./git-setup [OPTION]... [FILE]..." << std::endl;
-      return 1;
+  case -1:
+    std::cout << "Usage: ./git-setup [OPTION]... [FILE]..." << std::endl;
+    return 1;
   }
   std::string user;
   std::getline(std::cin, user);
